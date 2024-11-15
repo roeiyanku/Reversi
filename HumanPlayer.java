33be4686a395c35df630f5125f1dd8c12a7070ea
Roei Yanku <roeiyanku@gmail.com>
2024-11-15 17:16:17 +0200

    started initializing the Board itself

---
+++ HumanPlayer.java
@@ -0,0 +1,10 @@
+public class HumanPlayer extends AIPlayer{
+    public HumanPlayer(boolean isPlayerOne) {
+        super(isPlayerOne);
+    }
+
+    @Override
+    public Move makeMove(PlayableLogic gameStatus) {
+        return null;
+    }
+}
